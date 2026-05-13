35c8e03379c397c42bbb29a171a648dcc8f5a5b1
Дмитрий <dimabresky@gmail.com>
2026-05-14 01:10:19 +0300

    fix: validate required environment constants

--- local/php_interface/include/constants.php
+++ local/php_interface/include/constants.php
@@ -20,17 +20,31 @@ $dnkEnv = static function (string $name): string {
         return $value;
     }
 
-    if (isset($_ENV[$name])) {
+    if (array_key_exists($name, $_ENV)) {
         return (string)$_ENV[$name];
     }
 
-    return isset($_SERVER[$name]) ? (string)$_SERVER[$name] : '';
+    if (array_key_exists($name, $_SERVER)) {
+        return (string)$_SERVER[$name];
+    }
+
+    throw new RuntimeException(sprintf('Required environment variable "%s" is not set.', $name));
 };
 
-define('DNK_CATALOG_IBLOCK_ID', (int)$dnkEnv('DNK_CATALOG_IBLOCK_ID'));
+$dnkEnvInt = static function (string $name) use ($dnkEnv): int {
+    $value = trim($dnkEnv($name));
+
+    if ($value === '' || filter_var($value, FILTER_VALIDATE_INT) === false) {
+        throw new RuntimeException(sprintf('Required environment variable "%s" must be an integer.', $name));
+    }
+
+    return (int)$value;
+};
+
+define('DNK_CATALOG_IBLOCK_ID', $dnkEnvInt('DNK_CATALOG_IBLOCK_ID'));
 
 /** Инфоблок узкого промо-баннера в шапке (CODE dnk_header_promo). */
-define('DNK_HEADER_PROMO_IBLOCK_ID', (int)$dnkEnv('DNK_HEADER_PROMO_IBLOCK_ID'));
+define('DNK_HEADER_PROMO_IBLOCK_ID', $dnkEnvInt('DNK_HEADER_PROMO_IBLOCK_ID'));
 
 /** URL (GET): полный список данных по бонусам. */
 define('DNK_BONUS_ENDPOINT', $dnkEnv('DNK_BONUS_ENDPOINT'));
@@ -45,25 +59,25 @@ define('DNK_ORDER_EXPORT_LOGIN', $dnkEnv('DNK_ORDER_EXPORT_LOGIN'));
 define('DNK_ORDER_EXPORT_PASSWORD', $dnkEnv('DNK_ORDER_EXPORT_PASSWORD'));
 
 /** Сколько записей обрабатывать за один запуск агента. */
-define('DNK_ORDER_EXPORT_QUEUE_BATCH', (int)$dnkEnv('DNK_ORDER_EXPORT_QUEUE_BATCH'));
+define('DNK_ORDER_EXPORT_QUEUE_BATCH', $dnkEnvInt('DNK_ORDER_EXPORT_QUEUE_BATCH'));
 
 /** После стольких неудачных попыток статус E. */
-define('DNK_ORDER_EXPORT_MAX_ATTEMPTS', (int)$dnkEnv('DNK_ORDER_EXPORT_MAX_ATTEMPTS'));
+define('DNK_ORDER_EXPORT_MAX_ATTEMPTS', $dnkEnvInt('DNK_ORDER_EXPORT_MAX_ATTEMPTS'));
 
 /** Интервал агента в секундах (при PERIOD=Y). */
-define('DNK_ORDER_EXPORT_AGENT_INTERVAL', (int)$dnkEnv('DNK_ORDER_EXPORT_AGENT_INTERVAL'));
+define('DNK_ORDER_EXPORT_AGENT_INTERVAL', $dnkEnvInt('DNK_ORDER_EXPORT_AGENT_INTERVAL'));
 
 /** Сколько задач очереди бонусов по телефону за один запуск агента. */
-define('DNK_BONUS_BALANCE_QUEUE_BATCH', (int)$dnkEnv('DNK_BONUS_BALANCE_QUEUE_BATCH'));
+define('DNK_BONUS_BALANCE_QUEUE_BATCH', $dnkEnvInt('DNK_BONUS_BALANCE_QUEUE_BATCH'));
 
 /** После стольких неудачных попыток статус E. */
-define('DNK_BONUS_BALANCE_QUEUE_MAX_ATTEMPTS', (int)$dnkEnv('DNK_BONUS_BALANCE_QUEUE_MAX_ATTEMPTS'));
+define('DNK_BONUS_BALANCE_QUEUE_MAX_ATTEMPTS', $dnkEnvInt('DNK_BONUS_BALANCE_QUEUE_MAX_ATTEMPTS'));
 
 /** Интервал агента очереди бонусов (секунды, для регистрации в админке). */
-define('DNK_BONUS_BALANCE_AGENT_INTERVAL', (int)$dnkEnv('DNK_BONUS_BALANCE_AGENT_INTERVAL'));
+define('DNK_BONUS_BALANCE_AGENT_INTERVAL', $dnkEnvInt('DNK_BONUS_BALANCE_AGENT_INTERVAL'));
 
 /** Срок жизни начислений при импорте бонусов (лет). */
-define('DNK_BONUS_IMPORT_ACTIVE_YEARS', (int)$dnkEnv('DNK_BONUS_IMPORT_ACTIVE_YEARS'));
+define('DNK_BONUS_IMPORT_ACTIVE_YEARS', $dnkEnvInt('DNK_BONUS_IMPORT_ACTIVE_YEARS'));
 
 /** Код бонусной программы в ответе импорта (поле БонуснаяПрограммаЛояльности). */
 define('DNK_BONUS_IMPORT_PROGRAM_CODE', $dnkEnv('DNK_BONUS_IMPORT_PROGRAM_CODE'));
@@ -84,7 +98,7 @@ define('DNK_BONUS_JSON_KEY_PARTNER_PHONE', $dnkEnv('DNK_BONUS_JSON_KEY_PARTNER_P
 define('DNK_USER_REGISTER_EXPORT_ENDPOINT', $dnkEnv('DNK_USER_REGISTER_EXPORT_ENDPOINT'));
 
 /** Сколько задач очереди регистрации за один запуск агента. */
-define('DNK_USER_REGISTER_EXPORT_QUEUE_BATCH', (int)$dnkEnv('DNK_USER_REGISTER_EXPORT_QUEUE_BATCH'));
+define('DNK_USER_REGISTER_EXPORT_QUEUE_BATCH', $dnkEnvInt('DNK_USER_REGISTER_EXPORT_QUEUE_BATCH'));
 
 /** После стольких неудачных попыток статус E. */
-define('DNK_USER_REGISTER_EXPORT_MAX_ATTEMPTS', (int)$dnkEnv('DNK_USER_REGISTER_EXPORT_MAX_ATTEMPTS'));
+define('DNK_USER_REGISTER_EXPORT_MAX_ATTEMPTS', $dnkEnvInt('DNK_USER_REGISTER_EXPORT_MAX_ATTEMPTS'));
