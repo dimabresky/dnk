3d7df3474321c32b483c0ad1cd7f9de435c1b441
Дмитрий <dimabresky@gmail.com>
2026-06-03 17:05:35 +0300

    fix(bonus): remove line ending normalization from bonus content processing

--- local/php_interface/include/classes/BonusFetchAgent.php
+++ local/php_interface/include/classes/BonusFetchAgent.php
@@ -49,6 +49,7 @@ final class BonusFetchAgent
             return;
         }
 
+        $content = str_replace(["\r\n", "\r", "\n"], '', $content);
         $decoded = json_decode($content, true);
         if (!is_array($decoded)) {
             Utils::logClientBonusImportLine($logDir, $basename, '[error] invalid_json file=' . $basename);
