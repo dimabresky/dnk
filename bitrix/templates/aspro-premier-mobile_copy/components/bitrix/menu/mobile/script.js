4f7efe0727172348e133bd815f4ab722835535c1
Дмитрий <dimabresky@gmail.com>
2026-06-18 12:42:30 +0300

    fix(mobile-menu): register catalog menu click handler once

--- bitrix/templates/aspro-premier-mobile_copy/components/bitrix/menu/mobile/script.js
+++ bitrix/templates/aspro-premier-mobile_copy/components/bitrix/menu/mobile/script.js
@@ -1,14 +1,22 @@
-BX.Aspro.Utils.readyDOM(() => {
-  $(document).on(
-    'click',
-    '.mobilemenu__menu--catalog-wide .mobilemenu__menu-item--parent > .link-wrapper > .mobilemenu__item-link',
-    function (e) {
-      e.preventDefault();
+(function () {
+  if (window.__dnkMobileCatalogMenuBound) {
+    return;
+  }
 
-      const $toggle = $(this).siblings('.toggle_block');
-      if ($toggle.length) {
-        $toggle.trigger('click');
+  window.__dnkMobileCatalogMenuBound = true;
+
+  BX.Aspro.Utils.readyDOM(() => {
+    $(document).on(
+      'click',
+      '.mobilemenu__menu--catalog-wide .mobilemenu__menu-item--parent > .link-wrapper > .mobilemenu__item-link',
+      function (e) {
+        e.preventDefault();
+
+        const $toggle = $(this).siblings('.toggle_block');
+        if ($toggle.length) {
+          $toggle.trigger('click');
+        }
       }
-    }
-  );
-});
+    );
+  });
+})();
