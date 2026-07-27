f802c7fbe5246b63ee1f6930ab7632000dbad9e9
Дмитрий <dimabresky@gmail.com>
2026-07-27 23:42:32 +0300

    refactor: reuse metrics include path variable in headers

--- bitrix/templates/aspro-premier_copy/header.php
+++ bitrix/templates/aspro-premier_copy/header.php
@@ -12,17 +12,17 @@ $bIncludedModule = \Bitrix\Main\Loader::includeModule('aspro.premier');
 	<head>
                 <?
                 use Awz\CookiesSett\App as CookieApp;
-
+                $metricsFile = $_SERVER['DOCUMENT_ROOT'] . '/include/header/google_metrics.php';
                 if (\Bitrix\Main\Loader::includeModule('awz.cookiessett')) {
 
                     $app = CookieApp::getInstance();
                     if ($app->isEmpty() || $app->check(CookieApp::MARKET_EXT)) {
                         //разрешены маркетинговые
-                        include $_SERVER['DOCUMENT_ROOT'] . '/include/header/google_metrics.php';
+                        include $metricsFile;
                     }
 
                 } else {
-                    include $_SERVER['DOCUMENT_ROOT'] . '/include/header/google_metrics.php';
+                    include $metricsFile;
                 }
                 ?>
 
