12b21751fea1c0bbc2a4ee0875c9b8bcde0485f6
Дмитрий <dimabresky@gmail.com>
2026-07-27 23:15:33 +0300

    refactor: include GTM from google_metrics with AWZ consent

--- bitrix/templates/aspro-premier_copy/header.php
+++ bitrix/templates/aspro-premier_copy/header.php
@@ -18,17 +18,11 @@ $bIncludedModule = \Bitrix\Main\Loader::includeModule('aspro.premier');
                     $app = CookieApp::getInstance();
                     if ($app->isEmpty() || $app->check(CookieApp::MARKET_EXT)) {
                         //разрешены маркетинговые
-                        ?>
-                        <!-- Google Tag Manager -->
-                        <script data-skip-moving="true">(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                        })(window,document,'script','dataLayer','GTM-TL3G6JF8');</script>
-                        <!-- End Google Tag Manager -->
-                        <?
+                        include $_SERVER['DOCUMENT_ROOT'] . '/include/header/google_metrics.php';
                     }
 
+                } else {
+                    include $_SERVER['DOCUMENT_ROOT'] . '/include/header/google_metrics.php';
                 }
                 ?>
 
