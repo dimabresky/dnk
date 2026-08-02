5f6f8f3a979616830e00776c10a92a6bc55be18c
Дмитрий <dimabresky@gmail.com>
2026-08-02 16:35:47 +0300

    feat(search): add public search page and mobile page_blocks stub
    Expose /search with bitrix:search.page (Aspro search template) and add mobile template page_blocks placeholder.

---
+++ bitrix/templates/aspro-premier-mobile_copy/page_blocks/readme.php
@@ -0,0 +1,9 @@
+<? /*
+
+All files will be included from solution template path (module - aspro.premier):
+/bitrix/templates/aspro-premier/page_blocks/*
+
+Example:
+    - for include header mobile: file located in /bitrix/templates/aspro-premier/page_block/header_mobile_1.php
+    - for include header mobile menu: file located in /bitrix/templates/aspro-premier/page_block/header_mobile_menu_1.php
+*/?>
