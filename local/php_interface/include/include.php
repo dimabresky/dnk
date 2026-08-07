7a9f9d30d7494ba550e3958a2b481674ffe52ecf
Дмитрий <dimabresky@gmail.com>
2026-08-07 18:37:32 +0300

    feat: generate Facebook Meta product catalog RSS feed
    Add FacebookProductFeedAgent that writes upload/dnk_facebook_products_feed.xml with Meta-required fields, mirroring the Google feed agent.

--- local/php_interface/include/include.php
+++ local/php_interface/include/include.php
@@ -23,6 +23,7 @@ use Dnk\PhpInterface\FeedPictureQueueTable;
 use Dnk\PhpInterface\FeedPictureComposer;
 use Dnk\PhpInterface\FeedPictureService;
 use Dnk\PhpInterface\ProductExtendedReviewsAgent;
+use Dnk\PhpInterface\FacebookProductFeedAgent;
 use Dnk\PhpInterface\ProductFeedAgent;
 use Dnk\PhpInterface\ProfileBirthdayEvents;
 use Dnk\PhpInterface\UserAddEvents;
@@ -59,6 +60,7 @@ Loader::registerAutoLoadClasses(null, [
     OrderExportEvents::class => $classesPath . '/OrderExportEvents.php',
     ProductExtendedReviewsAgent::class => $classesPath . '/ProductExtendedReviewsAgent.php',
     ProductFeedAgent::class => $classesPath . '/ProductFeedAgent.php',
+    FacebookProductFeedAgent::class => $classesPath . '/FacebookProductFeedAgent.php',
     FeedPictureQueueTable::class => $classesPath . '/FeedPictureQueueTable.php',
     FeedPictureComposer::class => $classesPath . '/FeedPictureComposer.php',
     FeedPictureService::class => $classesPath . '/FeedPictureService.php',
